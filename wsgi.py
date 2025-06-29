91ca1aedfef92ff4be3c2bcf7ef3be549a9fd623
f1sherFM <kirillka229top@gmail.com>
2025-06-29 20:38:16 +0500

    Update 1.1.0

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from app import app
+import logging
+
+# Настройка логгера для production
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
